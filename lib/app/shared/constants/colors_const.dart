0a7204f1828531baaba5711f36e363cc07ba9e12
SamuelXimenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2023-03-07 14:22:13 -0300

    Add colors and string constants files

---
+++ lib/app/shared/constants/colors_const.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class ColorsConst {
+  static const Color black100 = Color.fromRGBO(0, 0, 0, 1);
+  static const Color black60 = Color.fromRGBO(0, 0, 0, 0.6);
+  static const Color black50 = Color.fromRGBO(0, 0, 0, 0.5);
+
+  static const Color white100 = Color.fromRGBO(255, 255, 255, 1);
+  static const Color white80 = Color.fromRGBO(255, 255, 255, 0.8);
+  static const Color white20 = Color.fromRGBO(255, 255, 255, 0.2);
+  static const Color white10 = Color.fromRGBO(255, 255, 255, 0.1);
+
+  // PRIMARY
+  static const Color primary1 = Color.fromRGBO(9, 56, 99, 1);
+  static const Color primary2 = Color.fromRGBO(59, 102, 164, 1);
+
+  static const Color terciary = Color.fromRGBO(96, 165, 250, 1);
+  static const Color terciary500 = Color.fromRGBO(59, 130, 246, 1);
+
+  static const Color infoColor = Color.fromRGBO(73, 190, 183, 1);
+  static const Color infoColor1 = Color.fromRGBO(5, 150, 105, 1);
+  static const Color infoColor2 = Color.fromRGBO(37, 99, 235, 1);
+  static const Color infoColor3 = Color.fromRGBO(30, 64, 175, 1);
+  static const Color infoColor4 = Color.fromRGBO(30, 58, 138, 1);
+  static const Color infoColor5 = Color.fromRGBO(5, 32, 56, 1);
+  static const Color infoColor6 = Color.fromRGBO(0, 23, 63, 1);
+  static const Color infoColor7 = Color.fromRGBO(17, 50, 69, 1);
+  static const Color infoColor8 = Color.fromRGBO(44, 93, 132, 1);
+  static const Color infoColor9 = Color.fromRGBO(69, 125, 170, 1);
+  static const Color infoColor10 = Color.fromRGBO(62, 195, 255, 1);
+  static const Color infoColor11 = Color.fromRGBO(18, 53, 124, 1);
+  static const Color infoColor12 = Color.fromRGBO(123, 97, 255, 1);
+  static const Color infoColor13 = Color.fromRGBO(7, 46, 81, 0.4);
+  static const Color infoColor14 = Color.fromRGBO(29, 65, 136, 1);
+  static const Color infoColor15 = Color.fromRGBO(29, 174, 239, 1);
+  static const Color infoColor16 = Color.fromRGBO(24, 73, 112, 1);
+
+  // WARNING
+  static const Color warning1 = Color.fromRGBO(246, 215, 114, 1);
+  static const Color warning2 = Color.fromRGBO(252, 211, 77, 1);
+  static const Color warning3 = Color.fromRGBO(251, 191, 36, 1);
+  static const Color warning4 = Color.fromRGBO(249, 115, 22, 1);
+  static const Color warning5 = Color.fromRGBO(220, 38, 38, 1);
+  static const Color warning6 = Color.fromRGBO(222, 87, 83, 1);
+  static const Color warning7 = Color.fromRGBO(239, 68, 68, 1);
+  static const Color warning8 = Color.fromRGBO(255, 8, 9, 1);
+
+  static const Color neutral700 = Color.fromRGBO(52, 65, 85, 1);
+  static const Color neutral400 = Color.fromRGBO(148, 163, 184, 1);
+  static const Color neutral300 = Color.fromRGBO(203, 213, 225, 1);
+  static const Color neutral100 = Color.fromRGBO(241, 245, 249, 1);
+  static const Color neutral50 = Color.fromRGBO(248, 250, 252, 1);
+  static const Color neutralColor1 = Color.fromRGBO(100, 116, 139, 1);
+  static const Color neutralColor2 = Color.fromRGBO(3, 19, 33, 1);
+  static const Color neutralColor3 = Color.fromRGBO(77, 85, 98, 1);
+  static const Color neutralColor4 = Color.fromRGBO(52, 65, 85, 1);
+  static const Color neutralColor5 = Color.fromRGBO(51, 65, 85, 1);
+  static const Color neutralColor6 = Color.fromRGBO(38, 50, 56, 1);
+  static const Color neutralColor7 = Color.fromRGBO(45, 45, 45, 0.6);
+  static const Color neutralColor8 = Color.fromRGBO(33, 41, 54, 1);
+  static const Color neutralColor9 = Color.fromRGBO(30, 41, 59, 1);
+  static const Color neutralColor10 = Color.fromRGBO(12, 12, 12, 0.4);
+  static const Color neutralColor11 = Color.fromRGBO(15, 23, 42, 1);
+  static const Color neutralColor12 = Color.fromRGBO(10, 9, 9, 1);
+  static const Color neutralColor13 = Color.fromRGBO(229, 231, 235, 1);
+  static const Color neutralColor14 = Color.fromRGBO(224, 224, 224, 1);
+  static const Color neutralColor15 = Color.fromRGBO(245, 245, 245, 1);
+  static const Color neutralColor16 = Color.fromRGBO(249, 250, 251, 1);
+  static const Color neutralColor17 = Color.fromRGBO(235, 235, 235, 1);
+  static const Color neutralColor18 = Color.fromRGBO(230, 230, 230, 1);
+  static const Color neutralColor19 = Color.fromRGBO(226, 232, 240, 1);
+  static const Color neutralColor20 = Color.fromRGBO(246, 246, 246, 1);
+  static const Color neutralColor21 = Color.fromRGBO(240, 240, 240, 1);
+  static const Color neutralColor22 = Color.fromRGBO(220, 220, 220, 1);
+  static const Color neutralColor23 = Color.fromRGBO(191, 191, 191, 1);
+  static const Color neutralColor24 = Color.fromRGBO(236, 243, 250, 1);
+  static const Color neutralColor25 = Color.fromRGBO(248, 251, 252, 1);
+  static const Color neutralColor26 = Color.fromRGBO(222, 222, 222, 1);
+  static const Color neutralColor27 = Color.fromRGBO(243, 244, 246, 1);
+  static const Color neutralColor28 = Color.fromRGBO(250, 250, 250, 1);
+  static const Color neutralColor29 = Color.fromRGBO(240, 242, 245, 1);
+  static const Color neutralColor30 = Color.fromRGBO(203, 214, 225, 1);
+  static const Color neutralColor31 = Color.fromRGBO(71, 85, 105, 1);
+  static const Color neutralColor32 = Color.fromRGBO(108, 108, 108, 1);
+  static const Color neutralColor33 = Color.fromRGBO(241, 245, 249, 0.4);
+}
---
+++ lib/app/shared/constants/string_const.dart
@@ -0,0 +1,13 @@
+class StringConst {
+  static const appTitle = 'Sys Móveis';
+
+  // Login Module
+  static const makeLogin = "Fazer Login";
+  static const email = 'E-mail';
+  static const password = 'Senha';
+  static const login = 'Entrar';
+
+  // Errors
+  static const emptyEmail = "Preencha com seu email!";
+  static const emptyPassword = "Preencha com sua senha!";
+}
