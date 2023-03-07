524d683eb4c205cfb62ac241ce736d11de505850
SamuelXimenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2023-03-07 14:42:57 -0300

    Add bottom navigation bar on home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:design_leveling/app/shared/constants/routes_const.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +12,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    int selectedIndex = 0;
+
+    void _onItemTapped(int index) {
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sys Móveis'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 38.84,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.location_on,
+              size: 38.84,
+            ),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.logout,
+              size: 38.84,
+            ),
+            label: 'Logout',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        backgroundColor: const Color(0xff3B66A4),
+        selectedItemColor: const Color(0xff49BEB7),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (selectedIndex) {
+          if (selectedIndex == 2) {
+            Modular.to.navigate(RoutesConst.login);
+          } else {
+            _onItemTapped(selectedIndex);
+          }
+        },
       ),
     );
   }
